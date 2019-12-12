aad1d47e1021a1dc36e66d7eac2b406c08bafbe5
Isaac Avilez <isaac1180@aol.com>
2019-12-12 19:50:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,7 +2,7 @@ class CashRegister
   
   attr_accessor :total
   
-  def initialize(total = 0, employee_discount)
-    @total = total
+  def initialize(total, employee_discount)
+    @total = 10
   end
 end #end of class
